ab38d117a4a9d89533435b7beb8492f5d316aac0
nitishsadire <nitishsandhu9@gmail.com>
2021-06-06 12:14:29 +0530

    changing dataset

--- Dataset.py
+++ Dataset.py
@@ -1,28 +1,42 @@
 from torch.utils.data.dataset import Dataset
 from torchvision import transforms
 import torch
-from Util import  get_img_sz, label_to_class
+from Util import label_to_class, transform
 from PIL import Image
 
 class MultiImageMultiBBoxDataset(Dataset):
-    def __init__(self, all_images, all_multi_bboxes, all_multi_labels):
+    def __init__(self, all_images, all_multi_bboxes, all_multi_labels, all_difficulties, all_indices, isTest=False, keep_difficult = False):
         self.images_list = all_images
         self.transform = transforms.Compose([   transforms.Resize((300, 300)),
                                                 transforms.ToTensor(),
                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
                                             ])
         self.multi_labels_list = all_multi_labels
+        self.isTest = isTest
         self.multi_bbox_list = all_multi_bboxes
+        self.all_difficulties = [torch.tensor(i) for i in all_difficulties]
+        self.keep_difficult = keep_difficult
+        self.all_indices = all_indices
 
     def __len__(self):
         return len(self.images_list)
 
     def __getitem__(self, index):
-        image = self.transform(Image.open(self.images_list[index]))
+        image = Image.open(self.images_list[index])
         c = torch.Tensor([label_to_class[i] for i in self.multi_labels_list[index]])
-        w, h = get_img_sz(self.images_list[index])
-        standardized_bbox = torch.Tensor(self.multi_bbox_list[index]) / torch.FloatTensor([w, h, w, h]).unsqueeze(0)
-        return image, c, standardized_bbox, index
+        bboxes = torch.Tensor(self.multi_bbox_list[index])
+
+        if self.keep_difficult == False:
+            bboxes = bboxes[self.all_difficulties[index]==0]
+            c = c[self.all_difficulties[index]==0]
+
+        if self.isTest==False:
+            image, bboxes, c = transform(image, bboxes, c)
+        w, h = image.size
+        standardized_bbox = bboxes/torch.FloatTensor([w, h, w, h]).unsqueeze(0)
+        image = self.transform(image)
+
+        return image, c, standardized_bbox, self.all_indices[index]
 
 def collate_fn(batch):
     images = []
